6917e6cf56939c343e43da3fad8422f160e73243
Valent-x7 <valentinvacacipres@gmail.com>
2026-09-06 17:14:25 -0600

    feat: implement authentication logic with useAuth hook and update LoginForm

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -5,3 +5,21 @@ export const loginUser = async (credentials : LoginRequest) : Promise<any> => {
     const response = await apiClient.post<any>("/api/auth/login", credentials);
     return response.data;
 }
+
+export const getUserSession = async () => {
+    if (typeof window !== "undefined" && !localStorage.getItem("token")) {
+        return null;
+    }
+
+    try {
+        const response = await apiClient.get("/api/auth/me");
+        return response.data.usuario;
+
+    } catch (error) {
+        if (typeof window !== "undefined") {
+            localStorage.removeItem("token");
+            localStorage.removeItem("refreshToken");
+        }
+        return null;
+    }
+}
