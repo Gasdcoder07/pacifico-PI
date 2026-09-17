7eb8d1f9f44e88f14c3e30373a8eb1fa206421b6
Gasdcoder07 <gserna@ucol.mx>
2026-09-16 18:59:04 -0600

    feat: implement admin registration endpoint and remove old register endpoint

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
+import pool from "@/lib/db";
 
 /**
  * @swagger
@@ -118,60 +119,5 @@ import { supabase } from "@/lib/supabase";
  *                   type: integer
  */
 export async function POST(request: Request) {
-    try {
-        const { email, password } = await request.json()
-
-        if (!email || !password) {
-            return NextResponse.json(
-                { error: "Error, hace faltan datos para poder entrar ( Usuario, Contraseña )" },
-                { status: 400 }
-            )
-        }
-
-        const { data: authData, error: authError } = await supabase.auth.signInWithPassword({
-            email,
-            password
-        })
-
-        if (authError || !authData) {
-            return NextResponse.json(
-                {   error: authError?.message,
-                    code: authError?.code,
-                    status: authError?.status },
-                { status: 401 }
-            )
-        }
-        const { data: perfilUsuario, error: perfilError } = await supabase
-            .from('usuarios')
-            .select('id, nombre,apellido, correo, rol_id, sucursal_id, estado')
-            .eq('auth_user_id', authData.user.id)
-            .single();
-
-        console.log('Perfil del usuario obtenido: ', perfilUsuario, 'Error al obtener el perfil: ', perfilError)
-
-        if (perfilError || !perfilUsuario) {
-            return NextResponse.json(
-                { error: 'No se encontró el perfil en la base de datos', detalle: perfilError?.message || 'Perfil no encontrado' },
-                { status: 404 }
-            )
-        }
-
-        if (!perfilUsuario.estado) {
-            return NextResponse.json(
-                { error: "La cuenta del usuario está inactiva" },
-                { status: 403 }
-            )
-        }
-
-        return NextResponse.json(
-            {message: 'Login exitoso', session: authData.session, usuario: perfilUsuario}, 
-                {status: 200}
-        )
-        
-    } catch(err) {
-        console.error('Error en el endpoint de login: ', err)
-        return NextResponse.json(
-            { error: `Error interno del servidor: ${err}`, status: 500 }
-        )
-    }
+    
 }
