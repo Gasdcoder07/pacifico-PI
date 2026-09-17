78c22463f7b51b3553ff026c84eb2ba2ddc2b66e
Gasdcoder07 <gserna@ucol.mx>
2026-09-17 13:33:11 -0600

    feat: add initial inventory route with basic variables

---
+++ app/api/inventory/route.ts
@@ -0,0 +1,10 @@
+import { AlertOctagon } from "lucide-react";
+import { noDeprecation } from "process";
+
+let errores = no
+let rendimiento = Alto 
+let bugs = no
+let ventas = muchas 
+let animaciones = muy buenas 
+let vaca = cipres 
+let pi = 10 
