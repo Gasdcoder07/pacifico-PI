81f523cef815d48c8f8c8821a755c5903bf206f3
Gasdcoder07 <gserna@ucol.mx>
2026-09-20 23:59:00 -0600

    refactor: update LoadingIndicator styles and remove deprecated skills-lock.json

--- shared/components/LoadingIndicator.tsx
+++ shared/components/LoadingIndicator.tsx
@@ -1,5 +1,5 @@
 export const LoadingIndicator = () => {
     return (
-        <div className="animate-spin border-b-4 size-6 border-white"/>
+        <div className="animate-spin border-b-4 rounded-full size-6 border-cyan-50"/>
     )
 }
